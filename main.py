cbaba687b4fc45e48c17fe0df1cb92af1465027a
ycn0320 <ycn0320@gmail.com>
2016-05-17 21:37:28 +0900

    dd

---
+++ main.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
+import webapp2
+
+# URL, JSON, 로그, 정규표현식 관련 라이브러리 로드
+import urllib
+import urllib2
+import json
+import logging
+import re
+
+# 봇 토큰, 봇 API 주소
+TOKEN = '192794280:AAFtJK70ZC2mPRH8uqwpx1-U2OwnQ8Bbzp4'
+BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
+
+# 봇이 응답할 명령어
+CMD_ROLL = '/roll'
+
+# 메시지 발송 관련 함수들
+def send_msg(chat_id, text, reply_to=None, no_preview=True, keyboard=None):
+    u"""send_msg: 메시지 발송
+    chat_id:    (integer) 메시지를 보낼 채팅 ID
+    text:       (string)  메시지 내용
+    reply_to:   (integer) ~메시지에 대한 답장
+    no_preview: (boolean) URL 자동 링크(미리보기) 끄기
+    keyboard:   (list)    커스텀 키보드 지정
+    """
+    params = {
+        'chat_id': str(chat_id),
+        'text': text.encode('utf-8'),
+        }
+    if reply_to:
+        params['reply_to_message_id'] = reply_to
+    if no_preview:
+        params['disable_web_page_preview'] = no_preview
+    if keyboard:
+        reply_markup = json.dumps({
+            'keyboard': keyboard,
+            'resize_keyboard': True,
+            'one_time_keyboard': False,
+            'selective': (reply_to != None),
+            })
+        params['reply_markup'] = reply_markup
+    try:
+        urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode(params)).read()
+    except Exception as e: 
+        logging.exception(e)
+
+def broadcast(text):
+    u"""broadcast: 봇이 켜져 있는 모든 채팅에 메시지 발송
+    text:       (string)  메시지 내용
+    """
+    for chat in get_enabled_chats():
+        send_msg(chat.key.string_id(), text)
+		
+# 봇 명령 처리 함수들
+def cmd_start(chat_id):
+    u"""cmd_start: 봇을 활성화하고, 활성화 메시지 발송
+    chat_id: (integer) 채팅 ID
+    """
+    send_msg(chat_id, MSG_START, keyboard=CUSTOM_KEYBOARD)
+	
+def process_cmds(msg):
+u"""사용자 메시지를 분석해 봇 명령을 처리
+chat_id: (integer) 채팅 ID
+text:    (string)  사용자가 보낸 메시지 내용
+"""
+msg_id = msg['message_id']
+chat_id = msg['chat']['id']
+text = msg.get('text')
+
+if (not text):
+	return
+if CMD_ROLL == text:
+	cmd_start(chat_id)
+	return
+	
+	
+# 웹 요청에 대한 핸들러 정의
+# /me 요청시
+class MeHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getMe'))))
+
+# /updates 요청시
+class GetUpdatesHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getUpdates'))))
+
+# /set-wehook 요청시
+class SetWebhookHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        url = self.request.get('url')
+        if url:
+            self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'setWebhook', urllib.urlencode({'url': url})))))
+
+# /webhook 요청시 (텔레그램 봇 API)
+class WebhookHandler(webapp2.RequestHandler):
+    def post(self):
+        urlfetch.set_default_fetch_deadline(60)
+        body = json.loads(self.request.body)
+        self.response.write(json.dumps(body))
+        process_cmds(body['message'])
+
+# 구글 앱 엔진에 웹 요청 핸들러 지정
+app = webapp2.WSGIApplication([
+    ('/me', MeHandler),
+    ('/updates', GetUpdatesHandler),
+    ('/set-webhook', SetWebhookHandler),
+    ('/webhook', WebhookHandler),
+], debug=True)
