e717648ff6a1349b266300c68d8a82014aa00346
창남 유 <ycn0320@gmail.com>
2016-05-17 13:13:54 +0000

    df

--- main.py
+++ main.py
@@ -1,198 +1,59 @@
-# -*- coding: cp949 -*-
-#!/usr/bin/env python
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+import StringIO
+import json
+import logging
+import random
+import urllib
+import urllib2
 
+# for sending images
+from PIL import Image
+import multipart
+
+# standard app engine imports
 from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import webapp2
 
-# URL, JSON, log, exp
-import urllib
-import urllib2
-import json
-import logging
-import re
+TOKEN = 'YOUR_BOT_TOKEN_HERE'
 
-# bot token, address
-TOKEN = '192794280:AAFtJK70ZC2mPRH8uqwpx1-U2OwnQ8Bbzp4'
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
-# bot command
-CMD_ROLL = '!주사위'
 
-CMD_START     = '/start'
-CMD_STOP      = '/stop'
-CMD_HELP      = '/help'
-CMD_BROADCAST = '/broadcast'
+# ================================
 
-# 봇 사용법 & 메시지
-USAGE = u"""[사용법] 아래 명령어를 메시지로 보내거나 버튼을 누르시면 됩니다.
-/start - (봇 활성화)
-/stop  - (봇 비활성화)
-/help  - (이 도움말 보여주기)
-"""
-MSG_START = u'봇을 시작합니다.'
-MSG_STOP  = u'봇을 정지합니다.'
-
-# 커스텀 키보드
-CUSTOM_KEYBOARD = [
-        [CMD_START],
-        [CMD_STOP],
-        [CMD_HELP],
-        ]
-
-# 채팅별 봇 활성화 상태
-# 구글 앱 엔진의 Datastore(NDB)에 상태를 저장하고 읽음
-# 사용자가 /start 누르면 활성화
-# 사용자가 /stop  누르면 비활성화
 class EnableStatus(ndb.Model):
-    enabled = ndb.BooleanProperty(required=True, indexed=True, default=False,)
+    # key name: str(chat_id)
+    enabled = ndb.BooleanProperty(indexed=False, default=False)
 
-def set_enabled(chat_id, enabled):
-    u"""set_enabled: 봇 활성화/비활성화 상태 변경
-    chat_id:    (integer) 봇을 활성화/비활성화할 채팅 ID
-    enabled:    (boolean) 지정할 활성화/비활성화 상태
-    """
+
+# ================================
+
+def setEnabled(chat_id, yes):
     es = EnableStatus.get_or_insert(str(chat_id))
-    es.enabled = enabled
+    es.enabled = yes
     es.put()
 
-def get_enabled(chat_id):
-    u"""get_enabled: 봇 활성화/비활성화 상태 반환
-    return: (boolean)
-    """
+def getEnabled(chat_id):
     es = EnableStatus.get_by_id(str(chat_id))
     if es:
         return es.enabled
     return False
 
-def get_enabled_chats():
-    u"""get_enabled: 봇이 활성화된 채팅 리스트 반환
-    return: (list of EnableStatus)
-    """
-    query = EnableStatus.query(EnableStatus.enabled == True)
-    return query.fetch()
 
-# 메시지 발송 관련 함수들
-def send_msg(chat_id, text, reply_to=None, no_preview=True, keyboard=None):
-    u"""send_msg: 메시지 발송
-    chat_id:    (integer) 메시지를 보낼 채팅 ID
-    text:       (string)  메시지 내용
-    reply_to:   (integer) ~메시지에 대한 답장
-    no_preview: (boolean) URL 자동 링크(미리보기) 끄기
-    keyboard:   (list)    커스텀 키보드 지정
-    """
-    params = {
-        'chat_id': str(chat_id),
-        'text': text.encode('utf-8'),
-        }
-    if reply_to:
-        params['reply_to_message_id'] = reply_to
-    if no_preview:
-        params['disable_web_page_preview'] = no_preview
-    if keyboard:
-        reply_markup = json.dumps({
-            'keyboard': keyboard,
-            'resize_keyboard': True,
-            'one_time_keyboard': False,
-            'selective': (reply_to != None),
-            })
-        params['reply_markup'] = reply_markup
-    try:
-        urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode(params)).read()
-    except Exception as e: 
-        logging.exception(e)
+# ================================
 
-def broadcast(text):
-    u"""broadcast: 봇이 켜져 있는 모든 채팅에 메시지 발송
-    text:       (string)  메시지 내용
-    """
-    for chat in get_enabled_chats():
-        send_msg(chat.key.string_id(), text)
-
-# 봇 명령 처리 함수들
-def cmd_start(chat_id):
-    u"""cmd_start: 봇을 활성화하고, 활성화 메시지 발송
-    chat_id: (integer) 채팅 ID
-    """
-    set_enabled(chat_id, True)
-    send_msg(chat_id, MSG_START, keyboard=CUSTOM_KEYBOARD)
-
-def cmd_stop(chat_id):
-    u"""cmd_stop: 봇을 비활성화하고, 비활성화 메시지 발송
-    chat_id: (integer) 채팅 ID
-    """
-    set_enabled(chat_id, False)
-    send_msg(chat_id, MSG_STOP)
-
-def cmd_help(chat_id):
-    u"""cmd_help: 봇 사용법 메시지 발송
-    chat_id: (integer) 채팅 ID
-    """
-    send_msg(chat_id, USAGE, keyboard=CUSTOM_KEYBOARD)
-
-def cmd_broadcast(chat_id, text):
-    u"""cmd_broadcast: 봇이 활성화된 모든 채팅에 메시지 방송
-    chat_id: (integer) 채팅 ID
-    text:    (string)  방송할 메시지
-    """
-    send_msg(chat_id, u'메시지를 방송합니다.', keyboard=CUSTOM_KEYBOARD)
-    broadcast(text)
-
-def cmd_echo(chat_id, text, reply_to):
-    u"""cmd_echo: 사용자의 메시지를 따라서 답장
-    chat_id:  (integer) 채팅 ID
-    text:     (string)  사용자가 보낸 메시지 내용
-    reply_to: (integer) 답장할 메시지 ID
-    """
-    send_msg(chat_id, text, reply_to=reply_to)
-
-def process_cmds(msg):
-    u"""사용자 메시지를 분석해 봇 명령을 처리
-    chat_id: (integer) 채팅 ID
-    text:    (string)  사용자가 보낸 메시지 내용
-    """
-    msg_id = msg['message_id']
-    chat_id = msg['chat']['id']
-    text = msg.get('text')
-    if (not text):
-        return
-    if CMD_START == text:
-        cmd_start(chat_id)
-        return
-    if (not get_enabled(chat_id)):
-        return
-    if CMD_STOP == text:
-        cmd_stop(chat_id)
-        return
-    if CMD_HELP == text:
-        cmd_help(chat_id)
-        return
-    cmd_broadcast_match = re.match('^' + CMD_BROADCAST + ' (.*)', text)
-    if cmd_broadcast_match:
-        cmd_broadcast(chat_id, cmd_broadcast_match.group(1))
-        return
-    cmd_echo(chat_id, text, reply_to=msg_id)
-    return
-
-# 웹 요청에 대한 핸들러 정의
-# /me 요청시
 class MeHandler(webapp2.RequestHandler):
     def get(self):
         urlfetch.set_default_fetch_deadline(60)
         self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getMe'))))
 
-# /updates 요청시
+
 class GetUpdatesHandler(webapp2.RequestHandler):
     def get(self):
         urlfetch.set_default_fetch_deadline(60)
         self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getUpdates'))))
 
-# /set-wehook 요청시
+
 class SetWebhookHandler(webapp2.RequestHandler):
     def get(self):
         urlfetch.set_default_fetch_deadline(60)
@@ -200,18 +61,84 @@ class SetWebhookHandler(webapp2.RequestHandler):
         if url:
             self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'setWebhook', urllib.urlencode({'url': url})))))
 
-# /webhook 요청시 (텔레그램 봇 API)
+
 class WebhookHandler(webapp2.RequestHandler):
     def post(self):
         urlfetch.set_default_fetch_deadline(60)
         body = json.loads(self.request.body)
+        logging.info('request body:')
+        logging.info(body)
         self.response.write(json.dumps(body))
-        process_cmds(body['message'])
 
-# 구글 앱 엔진에 웹 요청 핸들러 지정
+        update_id = body['update_id']
+        message = body['message']
+        message_id = message.get('message_id')
+        date = message.get('date')
+        text = message.get('text')
+        fr = message.get('from')
+        chat = message['chat']
+        chat_id = chat['id']
+
+        if not text:
+            logging.info('no text')
+            return
+
+        def reply(msg=None, img=None):
+            if msg:
+                resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({
+                    'chat_id': str(chat_id),
+                    'text': msg.encode('utf-8'),
+                    'disable_web_page_preview': 'true',
+                    'reply_to_message_id': str(message_id),
+                })).read()
+            elif img:
+                resp = multipart.post_multipart(BASE_URL + 'sendPhoto', [
+                    ('chat_id', str(chat_id)),
+                    ('reply_to_message_id', str(message_id)),
+                ], [
+                    ('photo', 'image.jpg', img),
+                ])
+            else:
+                logging.error('no msg or img specified')
+                resp = None
+
+            logging.info('send response:')
+            logging.info(resp)
+
+        if text.startswith('/'):
+            if text == '/start':
+                reply('Bot enabled')
+                setEnabled(chat_id, True)
+            elif text == '/stop':
+                reply('Bot disabled')
+                setEnabled(chat_id, False)
+            elif text == '/image':
+                img = Image.new('RGB', (512, 512))
+                base = random.randint(0, 16777216)
+                pixels = [base+i*j for i in range(512) for j in range(512)]  # generate sample image
+                img.putdata(pixels)
+                output = StringIO.StringIO()
+                img.save(output, 'JPEG')
+                reply(img=output.getvalue())
+            else:
+                reply('What command?')
+
+        # CUSTOMIZE FROM HERE
+
+        elif 'who are you' in text:
+            reply('telebot starter kit, created by yukuku: https://github.com/yukuku/telebot')
+        elif 'what time' in text:
+            reply('look at the corner of your screen!')
+        else:
+            if getEnabled(chat_id):
+                reply('I got your message! (but I do not know how to answer)')
+            else:
+                logging.info('not enabled for chat_id {}'.format(chat_id))
+
+
 app = webapp2.WSGIApplication([
     ('/me', MeHandler),
     ('/updates', GetUpdatesHandler),
-    ('/set-webhook', SetWebhookHandler),
+    ('/set_webhook', SetWebhookHandler),
     ('/webhook', WebhookHandler),
 ], debug=True)
