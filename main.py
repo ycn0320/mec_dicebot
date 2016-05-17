9978660caff9515a209d7aa462277256570315f7
source_server <source_server@example.com>
2016-05-17 14:07:06 +0000

    modify workspace

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class WebhookHandler(webapp2.RequestHandler):
         if text.startswith('/'):
           	if text == '/dice':
               	randVal = random.randint(0, 16777216)
-              	reply('DiceVal );                
+              	reply('DiceVal = ' + randVal);                
             elif text == '/start':
                 reply('Bot enabled')
                 setEnabled(chat_id, True)
