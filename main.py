80fbbf4989fa1623d74aad821f86bbea406c024e
source_server <source_server@example.com>
2016-05-17 15:49:55 +0000

    modify workspace

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ class WebhookHandler(webapp2.RequestHandler):
               
         if text.startswith('/'):
           cmd_dice = re.match('/dice', text)
-          print cmd_dice.group(2)
+          print cmd_dice.group()
           if cmd_dice and isNumber(cmd_dice.group(2)):
             rand = random.randint(1, cmd_dice.group(2))
             reply('DiceVal = %d' % rand)
