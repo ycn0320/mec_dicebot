99c08a813495b5840542513b54334864fd265c02
source_server <source_server@example.com>
2016-05-17 14:03:35 +0000

    modify workspace

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ class WebhookHandler(webapp2.RequestHandler):
             logging.info(resp)
               
         if text.startswith('/'):
-          
+          	if text == '/dice'
             if text == '/start':
                 reply('Bot enabled')
                 setEnabled(chat_id, True)
