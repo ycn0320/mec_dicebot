6f7445a2105ea51e91fdd8cff12cb1e3fbb1cea5
창남 유 <ycn0320@gmail.com>
2016-05-18 06:12:13 +0000

    serqer

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ class WebhookHandler(webapp2.RequestHandler):
           if cmd_dice and bool(int(cmd_dice.group(1))):
             rand = random.randint(1, int(cmd_dice.group(1)))
 
-            if len(dicDiceResult) > 1:
+            if len(dicDiceResult.keys()) > 1:
               item = dicDiceResult.items()
               reply(u'지금 1등은 [%s] 친구가 굴린 [%s] 에요' % (item[0][0], item[0][1]))
               dicDiceResult = { username : rand }                        
