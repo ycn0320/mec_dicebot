877b6144b6187b91a020d201457cfd9f12c0352c
source_server <source_server@example.com>
2016-05-18 06:41:14 +0000

    modify workspace

--- main.py
+++ main.py
@@ -81,8 +81,8 @@ class WebhookHandler(webapp2.RequestHandler):
         fr = message.get('from')
         chat = message['chat']
         chat_id = chat['id']
-        if int(
-        username = fr['username']
+        if int(chat_id) > 0:
+                  username = fr['username']
         
         global dicDiceResult
         
