37a48e3d05c35e344f9a7cd1e74089efd70fbd99
source_server <source_server@example.com>
2016-05-18 08:57:48 +0000

    modify workspace

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ class WebhookHandler(webapp2.RequestHandler):
               if cmd_dice and bool(abs(int(cmd_dice.group(1)))):
                 rand = random.randint(1, abs(int(cmd_dice.group(1))))
                 reply(u'우리 친구는 [%s] 이 나왔어요!' % rand)
-                      return
+                return
 #            nSize = len(dicDiceResult)
 #            if int(nSize) > 1:
 #              item = dicDiceResult.items()
