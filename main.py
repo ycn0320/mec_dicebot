331710780d8740218d3a99faaef491390728224f
source_server <source_server@example.com>
2016-05-18 09:47:01 +0000

    modify workspace

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ class WebhookHandler(webapp2.RequestHandler):
 
         def reply(msg=None):
             if msg:
+              
                 resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({
                   'chat_id': str(chat_id),
                   'text': msg.encode('utf-8'),
