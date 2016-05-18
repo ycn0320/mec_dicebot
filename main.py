5a6256e5b33903cbf7b2d4608b4b17adc739ce73
source_server <source_server@example.com>
2016-05-18 06:20:58 +0000

    modify workspace

--- main.py
+++ main.py
@@ -127,8 +127,8 @@ class WebhookHandler(webapp2.RequestHandler):
             else:
               dicDiceResult = { username : rand }
               reply(u'우리 [%s] 친구는 [%s] 이 나왔어요!' % (username, rand))
-            
-          
+              
+               
         
             return
 
