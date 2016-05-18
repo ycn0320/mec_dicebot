bcf803f0aec7a82ca767b93371ece209762eedfc
source_server <source_server@example.com>
2016-05-18 05:28:26 +0000

    modify workspace

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ class WebhookHandler(webapp2.RequestHandler):
               item = dicDiceResult.items()
               reply(u'지금 1등은 [%s] 친구가 굴린 [%s] 에요' % (item[0][0], item[0][1]))
               if item[0][0] < rand:
-                reply(u'와! 축하해요~ 우리 [%s] 친구가 [%s]로 1등이에요!' % (chat_username, rand))
+                reply(u'와! 축하해요~ 우리 [%s] 친구가 [%s]로 1등이에요!' % (username, rand))
               else:
                 reply(u'아...아깝네요.. 우리 [%s] 친구는 [%s]에요' % (chat_username, rand))                                
             else:
