0b465cbd267a8d2bdd4eb3d6d575b6e0d9aeb7f9
source_server <source_server@example.com>
2016-05-18 06:41:04 +0000

    modify workspace

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ class WebhookHandler(webapp2.RequestHandler):
         fr = message.get('from')
         chat = message['chat']
         chat_id = chat['id']
+        
         username = fr['username']
         
         global dicDiceResult
