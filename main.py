afd074ce64ad7cb6828ac9c9bc0a65c2f29204dd
source_server <source_server@example.com>
2016-05-18 02:30:47 +0000

    modify workspace

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ TOKEN = '192794280:AAFtJK70ZC2mPRH8uqwpx1-U2OwnQ8Bbzp4'
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
-dicDiceResult 
+dicDiceResult = { '' : '' }
 
 # ================================
 
