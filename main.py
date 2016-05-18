5165f88bb024232a7a4e71261797fa587c2f622d
창남 유 <ycn0320@gmail.com>
2016-05-18 05:57:00 +0000

    123123

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ class WebhookHandler(webapp2.RequestHandler):
             if len(dicDiceResult) > 1:
               item = dicDiceResult.items()
               reply(u'지금 1등은 [%s] 친구가 굴린 [%s] 에요' % (item[0][0], item[0][1]))
-              if item[0][0] < rand:
+              if int(item[0][1]) < rand:
                 reply(u'와! 축하해요~ 우리 [%s] 친구가 [%s]로 1등이에요!' % (username, rand))
               else:
                 reply(u'아...아깝네요.. 우리 [%s] 친구는 [%s]에요' % (username, rand))                                
