b514b3c4cdae7c87ded231f8ef30aefd75bec992
창남 유 <ycn0320@gmail.com>
2016-05-19 06:29:22 +0000

    12312123

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ class WebhookHandler(webapp2.RequestHandler):
           cmd_dice = re.match('^' + '/dice' + ' (.*)', text)
           try:
             inputVal = int(cmd_dice.group(1))
-            if cmd_dice and bool( abs(int(inputVal)) ):
+            if cmd_dice:
               rand = random.randint(1, abs(inputVal))
               reply(chat_id, u'우리 [%s] 친구는 [%s] 이 나왔어요!' % (username, rand))
           except Exception as e:
