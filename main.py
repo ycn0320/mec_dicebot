49113aa7071675bbca1e62d3089acb185aef0edf
source_server <source_server@example.com>
2016-05-19 06:53:08 +0000

    modify workspace

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ class WebhookHandler(webapp2.RequestHandler):
           setEnabled(chat_id, False)
           return
         #if getEnabled(chat_id):
-        if text.startwidth('!':
+        if text.startwidth('!'):
           cmd_dice = re.match('^' + '/dice' + ' (.*)', text)
           try:
             inputVal = int(cmd_dice.group(1))
