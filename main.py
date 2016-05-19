75ed7912b144b870cb184132240c409e6115f4e3
source_server <source_server@example.com>
2016-05-19 01:54:55 +0000

    modify workspace

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ class WebhookHandler(webapp2.RequestHandler):
 
         def reply(msg):
             if msg:
-              resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({ 'chat_id': str(chat_id), 'text': msg.encode('utf-8'), 'disable_web_page_preview': 'true', 'reply_to_message_id': str(message_id), })).read()
+              resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({ 'chat_id': str(chat_id), 'text': msg.encode('utf-8'), 'disable_web_page_preview': 'true', 'reply_to_message_id': str(message_id),             })).read()
             else:
                 resp = None
         
