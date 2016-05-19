ead358b7bcb38031f8a70af67868fd9c6335d08c
창남 유 <ycn0320@gmail.com>
2016-05-19 04:42:27 +0000

    1412124124

--- main.py
+++ main.py
@@ -89,23 +89,27 @@ class WebhookHandler(webapp2.RequestHandler):
         if not text:
             return
         
-        if text.startswith('/'):
-          if text == '/start':
-            reply(chat_id, u'굴려 굴려 주사위!')
-            setEnabled(chat_id, True)
+        if text == '/start':
+          reply(chat_id, u'굴려 굴려 주사위!')
+          setEnabled(chat_id, True)
+          return
+        if (not getEnabled(chat_id)):
+          return
+        if text == '/stop':
+          reply(chat_id, u'보고 또 보고, 매일 또 보기 약속!')
+          setEnabled(chat_id, False)
+          return
+        if getEnabled(chat_id):
+          cmd_dice = re.match('^' + '/dice' + ' (.*)', text)
+          if cmd_dice and bool(abs(int(cmd_dice.group(1)))):
+            rand = random.randint(1, abs(int(cmd_dice.group(1))))
+            reply(chat_id, u'우리 친구는 [%s] 이 나왔어요!' % rand)
             return
-          if (not getEnabled(chat_id)):
-            return
-          if text == '/stop':
-            reply(chat_id, u'보고 또 보고, 매일 또 보기 약속!')
-            setEnabled(chat_id, False)
-            return
-          if getEnabled(chat_id):
-            cmd_dice = re.match('^' + '/dice' + ' (.*)', text)
-            if cmd_dice and bool(abs(int(cmd_dice.group(1)))):
-              rand = random.randint(1, abs(int(cmd_dice.group(1))))
-              reply(chat_id, u'우리 친구는 [%s] 이 나왔어요!' % rand)
-              return
+          else
+          	return
+        else:
+          return
+        	
 
 app = webapp2.WSGIApplication([
     ('/me', MeHandler),
