b53c0ccf9de06680f75688156da59c97b6841dea
창남 유 <ycn0320@gmail.com>
2016-05-19 04:48:13 +0000

    123123

--- main.py
+++ main.py
@@ -48,7 +48,8 @@ def reply(chat_id, text):
         'chat_id': str(chat_id),
         'text': text.encode('utf-8'),
         }
-  urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode(params)).read()
+  result = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode(params))
+  print result.read()
 
 
 # ================================
