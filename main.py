f76bfcd0a09df59ddf55aad3e29f76ca890dde35
source_server <source_server@example.com>
2016-05-19 05:25:47 +0000

    modify workspace

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def reply(chat_id, text):
         'text': text.encode('utf-8'),
         }
   result = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode(params))
-  print result.read()
+  result.read()
 
 
 # ================================
