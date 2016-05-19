accefadde3ba8bd5d4e3786b9a834cccb0599939
창남 유 <ycn0320@gmail.com>
2016-05-19 05:48:50 +0000

    asdfasdf

--- main.py
+++ main.py
@@ -44,8 +44,10 @@ def reply(chat_id, text):
         'chat_id': str(chat_id),
         'text': text.encode('utf-8'),
         }
-  result = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode(params))
-  result.read()
+  try:
+    urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode(params)).read()
+  except Exception as e:
+    logging.exception(e)
 
 
 # ================================
