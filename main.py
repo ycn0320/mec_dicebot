8ad6cd2849e8fe310d9972bc9e6490854fa6aa31
source_server <source_server@example.com>
2016-05-19 06:36:28 +0000

    modify workspace

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ class WebhookHandler(webapp2.RequestHandler):
         chat_id = chat['id']
         try:
           #username = message['from']['username']
-          username = ch
+          username = chat['t
         except Exception as e:
           username = u'멍청이'
 
