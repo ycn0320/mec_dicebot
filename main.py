02314f255ae2fadfd999506b27dc55f580af9e56
source_server <source_server@example.com>
2016-05-19 04:41:30 +0000

    modify workspace

--- main.py
+++ main.py
@@ -89,9 +89,8 @@ class WebhookHandler(webapp2.RequestHandler):
         if not text:
             return
         
-        
-          if text == '/start':
-            reply(chat_id, u'굴려 굴려 주사위!')
+        if text == '/start':
+          reply(chat_id, u'굴려 굴려 주사위!')
             setEnabled(chat_id, True)
             return
           if (not getEnabled(chat_id)):
