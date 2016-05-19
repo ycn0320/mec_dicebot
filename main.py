e2df2389d86ee0a86e65e1f91bcaecce7e2f6ea3
source_server <source_server@example.com>
2016-05-19 06:08:38 +0000

    modify workspace

--- main.py
+++ main.py
@@ -108,8 +108,8 @@ class WebhookHandler(webapp2.RequestHandler):
           try:
             inputVal = abs(int(cmd_dice.group(1)))
           except Exception as e:
-            reply(chat_id, u'숫자만 입력해줘잉!')
-            
+            reply(chat_id, u'숫자만 입력해줘잉')
+                     
           if cmd_dice and bool(abs(int(cmd_dice.group(1)))):
             rand = random.randint(1, abs(int(cmd_dice.group(1))))
             reply(chat_id, u'우리 [%s] 친구는 [%s] 이 나왔어요!' % (username, rand))
