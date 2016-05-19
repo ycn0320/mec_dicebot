36bb967af0591b23f4ec4805afcb576f853f50eb
source_server <source_server@example.com>
2016-05-19 01:59:16 +0000

    modify workspace

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def reply(msg):
         'chat_id': str(chat_id),
         'text': msg.encode('utf-8'),
         }
+  urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode(params)).read()
             if msg:
               resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({ 'chat_id': str(chat_id), 'text': msg.encode('utf-8'), 'disable_web_page_preview': 'true', 'reply_to_message_id': str(message_id),})).read()
             else:
