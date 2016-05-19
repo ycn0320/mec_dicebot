e6c7dfcc9ef8176f3976361f5bc671f3d8c6d2f6
source_server <source_server@example.com>
2016-05-19 02:02:48 +0000

    modify workspace

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ class WebhookHandler(webapp2.RequestHandler):
         
         if text.startswith('/'):
           if text == '/start':
-            reply(chat_id, u'굴려 굴려!')
+            reply(chat_id, u'굴려 굴려 주사위!')
             setEnabled(chat_id, True)
             return
           if text == '/stop':
